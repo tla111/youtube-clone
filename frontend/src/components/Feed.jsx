33e9bef98b799601b7657a00feca47399cda1a14
Tim La <tlamarketing111@gmail.com>
2022-11-21 14:03:19 -0500

    created sidebar component

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import { Sidebar } from './index'
 
 const Feed = () => {
     return (
-        <div>Feed</div>
+        <Stack sc={{ flexDirection: { sx: 'column', md: 'row' } }}>
+            <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+                <Sidebar />
+                <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
+                    Copyright 2022 TLA Media
+                </Typography>
+            </Box>
+        </Stack>
     )
 }
 
