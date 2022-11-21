81bce1c8763e1d43adb131497d6e8986b1b12c69
Tim La <tlamarketing111@gmail.com>
2022-11-21 13:57:50 -0500

    searchbar created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { logo } from '../utils/constants'
+import { SearchBar } from './index'
 
 const Navbar = () => (
     <Stack
@@ -13,6 +14,7 @@ const Navbar = () => (
         <Link to="/" style={{ display: "flex", alignItems: "center" }}>
             <img src={logo} alt="logo" height={45} />
         </Link>
+        <SearchBar />
     </Stack>
 )
 
