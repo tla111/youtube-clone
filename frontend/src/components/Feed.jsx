4cc7fb553fac4603ab0e601791bcf57dfcdf255b
Tim La <tlamarketing111@gmail.com>
2022-11-21 14:15:21 -0500

    feed layout fixed

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -4,11 +4,16 @@ import { Sidebar } from './index'
 
 const Feed = () => {
     return (
-        <Stack sc={{ flexDirection: { sx: 'column', md: 'row' } }}>
+        <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
             <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
                 <Sidebar />
                 <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
                     Copyright 2022 TLA Media
+                </Typography>
+            </Box>
+            <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+                <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+                    New <span style={{ color: "#FC1503" }}>videos</span>
                 </Typography>
             </Box>
         </Stack>
