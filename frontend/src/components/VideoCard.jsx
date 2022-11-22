6402deda72f644b87235f47a2982d09c36492d3e
Tim La <tlamarketing111@gmail.com>
2022-11-22 12:17:52 -0500

    imported imports for videocard component

---
+++ frontend/src/components/VideoCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
+
+const VideoCard = () => {
+    return (
+        <div>VideoCard</div>
+    )
+}
+
+export default VideoCard
