159324608f3f4aaa9627ecc3fe4fe7b35d8e4388
Tim La <tlamarketing111@gmail.com>
2022-11-22 12:05:36 -0500

    able to selected new category in sidebar

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -15,14 +15,17 @@ const Feed = () => {
     return (
         <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
             <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-                <Sidebar />
+                <Sidebar
+                    selectedCategory={selectedCategory}
+                    setSelectedCategory={setSelectedCategory}
+                />
                 <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
                     Copyright 2022 TLA Media
                 </Typography>
             </Box>
             <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
                 <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-                    New <span style={{ color: "#FC1503" }}>videos</span>
+                    {selectedCategory}  <span style={{ color: "#FC1503" }}>Videos</span>
                 </Typography>
 
                 <Videos video={[]} />
