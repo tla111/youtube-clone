d0c709af3fde8511a5b002223101a72cef8b1190
Tim La <tlamarketing111@gmail.com>
2022-11-23 08:24:44 -0500

    css gradient box appearing

--- frontend/src/components/ChannelDetail.jsx
+++ frontend/src/components/ChannelDetail.jsx
@@ -28,7 +28,15 @@ const ChannelDetail = () => {
     console.log(videos);
 
     return (
-        <div>{id}</div>
+        <Box minHeight="95vh">
+            <Box>
+                <div style={{
+                    height: '300px',
+                    background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+                    zIndex: 10,
+                }} />
+            </Box>
+        </Box>
     )
 }
 
