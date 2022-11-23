084298cf0c81eff55346b8d3c61cba9c55006b39
Tim La <tlamarketing111@gmail.com>
2022-11-23 08:55:12 -0500

    able to play clicked video

--- frontend/src/components/VideoDetail.jsx
+++ frontend/src/components/VideoDetail.jsx
@@ -8,12 +8,21 @@ import { Videos, Loader } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
+    const [videoDetail, setVideoDetail] = useState(null)
+    const { id } = useParams()
+
+    useEffect(() => {
+        fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+            .then((data) => setVideoDetail(data.items[0]))
+
+    }, [id])
+
     return (
         <Box minHeight="95vh">
             <Stack direction={{ xs: "column", md: "row" }}>
                 <Box flex={1}>
                     <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
-                        {/* <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls /> */}
+                        <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
                     </Box>
                 </Box>
 
