569b1dc564e447cb139ad085ba6ea09173863f4a
Tim La <tlamarketing111@gmail.com>
2022-11-23 08:43:01 -0500

    retrieving searchTerm in SearchBar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -4,10 +4,22 @@ import { Paper, IconButton } from '@mui/material'
 import { Search } from '@mui/icons-material'
 
 const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState("")
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (searchTerm) {
+            navigate(`/search/${searchTerm}`);
+
+            setSearchTerm('');
+        }
+    }
     return (
         <Paper
             component='form'
-            onSubmit={() => { }}
+            onSubmit={handleSubmit}
             sx={{
                 borderRadius: 20,
                 border: '1px solid #e3e3e3',
@@ -16,7 +28,7 @@ const SearchBar = () => {
                 mr: { sm: 5 }
             }}
         >
-            <input className='search-bar' placeholder='Search...' value='' onChange={() => { }} />
+            <input className='search-bar' placeholder='Search...' value={searchTerm} onChange={(e) => { setSearchTerm(e.target.value) }} />
             <IconButton type='submit' sx={{ p: '10px', color: 'red' }}>
                 <Search />
             </IconButton>
--- frontend/src/components/SearchFeed.jsx
+++ frontend/src/components/SearchFeed.jsx
@@ -3,23 +3,25 @@ import { Box, Typography } from '@mui/material'
 import { Videos } from './index'
 
 import { fetchFromAPI } from '../utils/fetchFromAPI'
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
 
     const [videos, setVideos] = useState([])
+    const { searchTerm } = useParams()
 
     useEffect(() => {
-        fetchFromAPI(`search?part=snippet&q=}`).then((data) => {
+        fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
             setVideos(data.items)
         })
-    }, [])
+    }, [searchTerm])
 
 
     return (
 
         <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
             <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-                Search Results for:  <span style={{ color: "#FC1503" }}>Videos</span>
+                Search Results for:  <span style={{ color: "#FC1503" }}>{searchTerm}</span>
             </Typography>
 
             <Videos videos={videos} />
