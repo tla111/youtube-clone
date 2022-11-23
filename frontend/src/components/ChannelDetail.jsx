7bf2e71263d2cbcf2e27e2bd9f7eb6f405aad315
Tim La <tlamarketing111@gmail.com>
2022-11-23 08:20:27 -0500

    able to fetch channel & videos details

--- frontend/src/components/ChannelDetail.jsx
+++ frontend/src/components/ChannelDetail.jsx
@@ -6,8 +6,29 @@ import { Videos, ChannelCard } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
+    const [channelDetail, setChannelDetail] = useState()
+    const [videos, setVideos] = useState(null)
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        const fetchResults = async () => {
+            const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+
+            setChannelDetail(data?.items[0]);
+
+            const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+
+            setVideos(videosData?.items);
+        };
+
+        fetchResults();
+    }, [id])
+
+    console.log(videos);
+
     return (
-        <div>ChannelDetail</div>
+        <div>{id}</div>
     )
 }
 
