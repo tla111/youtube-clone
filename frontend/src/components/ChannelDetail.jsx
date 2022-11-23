ccc0028f59566762e8bc06f813062b635eed256f
Tim La <tlamarketing111@gmail.com>
2022-11-23 08:30:37 -0500

    passed props for reusable components

--- frontend/src/components/ChannelDetail.jsx
+++ frontend/src/components/ChannelDetail.jsx
@@ -35,6 +35,7 @@ const ChannelDetail = () => {
                     background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
                     zIndex: 10,
                 }} />
+                <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
             </Box>
         </Box>
     )
