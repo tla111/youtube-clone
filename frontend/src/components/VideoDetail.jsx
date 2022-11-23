b0916cb71967029084aea940598a044a01d65cd8
Tim La <tlamarketing111@gmail.com>
2022-11-23 08:59:50 -0500

    able to render channel title

--- frontend/src/components/VideoDetail.jsx
+++ frontend/src/components/VideoDetail.jsx
@@ -17,6 +17,8 @@ const VideoDetail = () => {
 
     }, [id])
 
+    if (!videoDetail?.snippet) return "Loading";
+
     const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = videoDetail;
 
     return (
@@ -26,6 +28,17 @@ const VideoDetail = () => {
                     <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
                         <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
                     </Box>
+                    <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+                        {title}
+                    </Typography>
+                    <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2} >
+                        <Link to={`/channel/${channelId}`}>
+                            <Typography variant={{ sm: "subtitle1", md: 'h6' }} color="#fff" >
+                                {channelTitle}
+                                <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+                            </Typography>
+                        </Link>
+                    </Stack>
                 </Box>
 
             </Stack>
