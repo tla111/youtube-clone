979cc2f1fcbf48eb091ea79fe1ce262b4d08fc2c
Tim La <tlamarketing111@gmail.com>
2022-11-23 08:50:48 -0500

    imported imports for videoDetail

--- frontend/src/components/VideoDetail.jsx
+++ frontend/src/components/VideoDetail.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 const VideoDetail = () => {
     return (
